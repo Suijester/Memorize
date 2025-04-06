ee4f062beb998970386dcfd1fc5b3db467a459ac
Suijester <akilesh.yes@gmail.com>
2025-04-05 17:59:22 -0700

    added model

--- initializeModels.py
+++ initializeModels.py
@@ -4,3 +4,4 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 whisper.load_model("base", device = "cpu");
+model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
